a11641d4ca76deeab005ce99aeaa510cd52325b4
aimforkim <aimforkim@users.noreply.github.com>
2022-11-23 13:20:36 +0000

    new publisher node and changes in trajectory handler to publish pose array

--- trajectory_tools/scripts/test1_run_ghposes.py
+++ trajectory_tools/scripts/test1_run_ghposes.py
@@ -9,10 +9,9 @@ from trajectory_tools.trajectory_handler import TrajectoryHandler
 import tf.transformations
 from typing import List
 
-
 def pose_from_list(pose_list: List[float]) -> Pose:
     pose = Pose(
-        position=Point(pose_list[0], pose_list[1], pose_list[2]), 
+        position=Point(-(pose_list[0]), pose_list[1], pose_list[2]), 
         orientation=Quaternion(pose_list[3], pose_list[4], pose_list[5], pose_list[6]))
     return pose
     
@@ -28,12 +27,9 @@ def robot_program():
 
     pose_list = rospy.get_param('gh_poses')
     pose_goals = [pose_from_list(pose)for pose in pose_list]
-    
-
-    # th.publish_pose_array(pose_goals)
-    th.publish_poses_as_pose_array(pose_goals)
 
     sequence.append(Ptp(goal=start))
+
     # sequence.append(
     #     Lin(
     #         goal=Pose(
@@ -46,10 +42,10 @@ def robot_program():
 
     for pose_goal in pose_goals:
         sequence.append(Lin(goal=(pose_goal), vel_scale = 0.1, acc_scale = 0.05))
-    
+
     
     sequence.append(Ptp(goal=(start), vel_scale=0.2, acc_scale=0.1))
-    
+
     th.sequencer.plan(sequence)
 
     th.display_trajectory()
