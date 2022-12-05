131f1204a5486acf712a3e3b9bd7695f7d2c053b
aimforkim <aimforkim@users.noreply.github.com>
2022-12-05 13:50:02 +0000

    modified scripts

--- trajectory_tools/scripts/test2_run_ghposes.py
+++ trajectory_tools/scripts/test2_run_ghposes.py
@@ -48,7 +48,7 @@ def robot_program():
         sequence.append(Lin(goal=(pose_goal), vel_scale = 0.1, acc_scale = 0.05))
     
     
-    sequence.append(Ptp(goal=(start), vel_scale=0.2, acc_scale=0.1))
+    sequence.append(Ptp(goal=(start), vel_scale=0.2, acc_scale=0.2))
     
     th.sequencer.plan(sequence)
 
